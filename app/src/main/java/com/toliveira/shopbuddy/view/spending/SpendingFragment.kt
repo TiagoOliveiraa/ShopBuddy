93cdc4280e2b65b1afa1f4a33a513af78d006b97
Mythicale <tiagofilipedoliveira@gmail.com>
2023-09-18 22:43:38 +0100

    Make some of the Spending UI, learn how to use a library about charts

--- app/src/main/java/com/toliveira/shopbuddy/view/spending/SpendingFragment.kt
+++ app/src/main/java/com/toliveira/shopbuddy/view/spending/SpendingFragment.kt
@@ -1,11 +1,13 @@
 package com.toliveira.shopbuddy.view.spending
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.toliveira.shopbuddy.databinding.FragmentSpendingBinding
+import ir.mahozad.android.PieChart
 
 class SpendingFragment : Fragment() {
 
@@ -16,7 +18,24 @@ class SpendingFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSpendingBinding.inflate(inflater,container,false)
+        binding = FragmentSpendingBinding.inflate(inflater, container, false)
+
+
+        binding.pieChart.apply {
+            slices = listOf(
+                PieChart.Slice(0.2f, Color.BLUE, Color.BLUE, "test"),
+                PieChart.Slice(0.5f, Color.GREEN),
+                PieChart.Slice(0.3f, Color.YELLOW)
+            )
+            labelType = PieChart.LabelType.OUTSIDE
+            startAngle = -90
+            gradientType = PieChart.GradientType.SWEEP
+            holeRatio = 0f
+        }
+
+
+
+
         return binding.root
     }
 }
